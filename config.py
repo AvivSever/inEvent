84ea97deb72aac1ba9044558e1b342eddbcb6eb2
Aviv Sever <aviv.sever23@gmail.com>
2017-09-08 02:02:01 +0300

    Config file with connection string

---
+++ config.py
@@ -0,0 +1,2 @@
+# MongoDB
+CONNECTION_STRING = r"mongodb://avivsever:BLRKer1KzPAzGQkx@cluster0-shard-00-00-slp5m.mongodb.net:27017,cluster0-shard-00-01-slp5m.mongodb.net:27017,cluster0-shard-00-02-slp5m.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin"
