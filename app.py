064a007cff638f0fc9aa17ceb6015c7b82a456a1
AvivSever <aviv.sever23@gmail.com>
2017-09-08 01:46:29 +0300

    API complete - before MongoDB

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 # pylint: disable=invalid-name, C0111, C0301
-from flask import Flask, jsonify, abort, make_response, request
+from flask import Flask, jsonify, abort, make_response, request, url_for, redirect
+from flask_pymongo import PyMongo
+from flask_restful import Api, Resource
+from flask_httpauth import HTTPBasicAuth
 
+auth = HTTPBasicAuth()
 app = Flask(__name__)
 
 # TODO 1: Create a schema for an event
@@ -23,6 +27,35 @@ events = [
     }
 ]
 
+users = [
+    {
+        'username': 'don',
+        'password': 'juan',
+        'email': 'donjuan@gmail.com',
+        'permission': 'admin'
+    },
+    {
+        'username': 'eric',
+        'password': 'einstein',
+        'email': 'eeric@gmail.com',
+        'permission': 'basic'
+    }
+]
+
+
+@auth.get_password
+def get_password(username):
+    user = [user for user in users if user['username'] == username]
+    # if len(user) == 0:
+        # abort(401)
+    if len(user) != 0:
+        return user[0]['password']
+    return None
+
+@auth.error_handler
+def unauthorized():
+    return make_response(jsonify({'error': 'Unauthorized access'}), 403)
+    
 
 @app.route('/')
 def hello_world():
@@ -31,6 +64,7 @@ def hello_world():
 
 
 @app.route('/inevent/api/v1.0/events', methods=['GET'])
+@auth.login_required
 def get_events():
     """Returns to the client all the events available"""
     return jsonify({'events': events})
@@ -49,7 +83,7 @@ def get_event(event_id):
 
 @app.errorhandler(404)
 def not_found(error):
-    return make_response(jsonify({'error': 'Not Found', 'external information': str(error)}), 404)
+    return make_response(jsonify({'error': 'Not Found', 'info': str(error)}), 404)
 
 @app.route('/inevent/api/v1.0/events/create', methods=['POST'])
 def create_event():
@@ -58,8 +92,44 @@ def create_event():
         abort(400)
     new_event = {
         'id': events[-1]['id'] + 1,
-        'title': request.json['title']
+        'title': request.json['title'],
+        'description': request.json.get('description', ""),
+        'location': request.json.get('location', ""),
+        'secret': False
     }
+    events.append(new_event)
+    return jsonify({'event': new_event}), 201
+
+@app.route('/inevent/api/v1.0/events/update/<int:event_id>', methods=['PUT'])
+def update_event(event_id):
+    event = [event for event in events if event['id'] == event_id]
+    print 'Event -', event
+    print 'Type -', type(event)
+    if len(event) == 0:
+        abort(404)
+    if not request.json:
+        abort(400)
+    if 'title' in request.json and not isinstance(request.json['title'], unicode):
+        abort(400)
+    if 'description' in request.json and not isinstance(request.json['description'], unicode):
+        abort(400)
+    if 'location' in request.json and not isinstance(request.json['location'], unicode):
+        abort(400)
+    if 'secret' in request.json and not isinstance(request.json['secret'], bool):
+        abort(400)
+    event[0]['title'] = request.json.get('title', event[0]['title'])
+    event[0]['description'] = request.json.get('description', event[0]['description'])
+    event[0]['location'] = request.json.get('location', event[0]['location'])
+    event[0]['secret'] = request.json.get('secret', event[0]['secret'])
+    return jsonify({'result': True})
+
+@app.route('/inevent/api/v1.0/events/delete/<int:event_id>', methods=['DELETE'])
+def delete_event(event_id):
+    event = [event for event in events if event['id'] == event_id]
+    if len(event) == 0:
+        abort(404)
+    events.remove(event[0])
+    return jsonify({'result': True})
 
 if __name__ == '__main__':
     app.run(debug=True)
